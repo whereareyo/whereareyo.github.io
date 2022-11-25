f70257460be465f07417d722f8dc472af9a327b8
whereareyo <45404516+whereareyo@users.noreply.github.com>
2022-11-25 18:14:58 +0600

    Add files via upload

---
+++ Hello.js
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HelloWorld{
+         public static void main(String []args){
+            System.out.println("Hello, World!");
+            Scanner input = new Scanner(System.in);
+            int lab_0 = 0;
+            String lab_1 = input.nextLine();
+     }
+}
